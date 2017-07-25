2b77323d2b238bd4138ae9e93771eb6f2b799709
Your Name <you@example.com>
2017-07-25 17:20:16 +0300

    add tabs

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
+    $(".products-slider").owlCarousel({
+        center: true,
+        loop: true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            768:{
+                items:6,
+            },
+            992:{
+                items:6,
+            }
+        }
+    });
+
     $(".products-carousel").owlCarousel({
         responsiveClass:true,
         responsive:{
@@ -29,3 +46,20 @@ $(document).ready(function(){
         }
     });
 });
+
+function openSlider(evt, sliderName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(sliderName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
