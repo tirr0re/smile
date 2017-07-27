20b78c743bdccc0b26719da05374a44897253008
Your Name <you@example.com>
2017-07-27 11:00:45 +0300

    change from gulp to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+// This shows a full config file!
+module.exports = function (grunt) {
+    grunt.initConfig({
+        watch: {
+            files: 'app/less/**/*.less',
+            tasks: ['less']
+        },
+        less: {
+            dev: {
+                options: {
+                    plugins: [
+                        new (require('less-plugin-autoprefix'))({browsers: ["last 10 versions"]})
+                    ]
+                },
+                files: {
+                    'app/css/style.css': 'app/less/style.less'
+                }
+            }
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'app/css/*.css',
+                        'app/*.html',
+                        'app/js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './app'
+                }
+            }
+        }
+    });
+
+    // load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    // define default task
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
